4ec65f9f16f9a31ae4ffd24043349921014e75c1
Mike <art_is_me@mail.ru>
2026-02-15 22:48:53 +0300

    radix

--- labs/lab1/2/v6_radix/radix.cpp
+++ labs/lab1/2/v6_radix/radix.cpp
@@ -1,9 +1,14 @@
+//2147483647
+//(-2147483647 - 1)
+//-10000000000000000000000000000000
+//01111111111111111111111111111111
 //g++ radix.cpp -o radix.exe
 //radix.exe <from> <to> <value>
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <map>
+#include <limits>
 
 using namespace std;
 
@@ -15,16 +20,25 @@ int stringToInt(const string& str, int radix)
     bool negative = false;
     for (char ch : str)
     {
-        if (ch == '-') 
+        if (ch == '-' && !negative) 
         {
             negative = true;
             continue;
         }
-        if (size_t pos = RADIXES.find(ch); radix > pos && 
-            pos != string::npos) num = num * radix + pos;
-        else throw runtime_error("WRONG VALUE");
+        if (int pos = RADIXES.find(ch); radix > pos && pos != string::npos) 
+        {
+            if (negative)
+            {
+                if ((INT_MIN + pos) / 10  <= num) num = num * radix - pos;
+                else throw runtime_error("INT Overflow");
+            }
+            else
+                if ((INT_MAX - pos) / 10  >= num) num = num * radix + pos;
+                else throw runtime_error("INT Overflow");
+        }
+        else throw runtime_error("INVALID VALUE");
     }
-    return negative ? -num : num;
+    return num;
 }
 
 int getRadixFromString(string str)
@@ -40,36 +54,38 @@ int getRadixFromString(string str)
 
 string intToString(int n, int radix)
 {
+    if (n == 0) return "0";
+
     bool negative = false;
-    if (n < 0) {negative = true; n = -n;}
+    if (n < 0) negative = true;
+
     string num = "";
+    num.reserve(33);
     while (n != 0)
     {
-        num += RADIXES[n % radix];
+        num += RADIXES[abs(n % radix)];
         n = n / radix;
     }
-    negative ? num += '-' : num;
+    if (negative) num += '-';
     reverse(num.begin(), num.end());
     return num;
 }
 
 int main(int argc, char const *argv[])
 {
-    string fromArg = "10";
-    string toArg = "35";
-    string valueArg = "255";
-
-    try
-    {
-        int from = getRadixFromString(fromArg);
-        int to = getRadixFromString(toArg);
-        int value = stringToInt(valueArg, from);
-        cout << "Final:" << intToString(value, to) << endl;
-    }
-    catch(const std::exception& e)
-    {
-        cout << e.what() << endl;
-    }
-
+    if (argc == 4)
+        try
+        {
+            int from = getRadixFromString(argv[1]);
+            int to = getRadixFromString(argv[2]);
+            int value = stringToInt(argv[3], from);
+            cout << intToString(value, to) << endl;
+        }
+        catch(const std::exception& e)
+        {
+            cout << e.what() << endl;
+        }
+    else
+        cout << "INVALID NUMBER OF ARGS";
     return 0;
 }
